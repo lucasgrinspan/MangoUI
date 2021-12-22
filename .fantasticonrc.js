d5cdb750d0ca58e3db4aa92d3cdb0867618a45cd
Lucas Grinspan <lucasgrinspan@gmail.com>
2021-12-21 22:10:32 -0500

    feat(mangoUI): init

---
+++ .fantasticonrc.js
@@ -0,0 +1,17 @@
+// build file for font icons
+module.exports = {
+  name: "mango-icons",
+  inputDir: "assets/icons",
+  outputDir: "assets/font",
+  fontTypes: ["woff2"],
+  assetTypes: ["scss"],
+  fontsUrl: "/assets/font",
+  pathOptions: {
+    scss: "src/tokens/_icons.scss",
+  },
+  templates: {
+    scss: "templates/scss-icon.hbs",
+  },
+  tag: "span",
+  selector: ".icon",
+};
